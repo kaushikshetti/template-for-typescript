e7917ab75276497aa3040a7fe7d1d304e9ce9006
Kaushik Shetti <kshetti29@gmail.com>
2023-04-17 10:50:39 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({
+      message: "First Deployment!",
+    });
+  });
+
+const PORT = 3000;
+try {
+  app.listen(PORT, (): void => {
+    console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
